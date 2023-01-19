b93c94939b26a491145133e6f474eb094d60b911
Dean Parkin <Deanparkin@live.co.uk>
2023-01-19 18:12:40 +0000

    next 13 upgrade

--- components/Nav.js
+++ components/Nav.js
@@ -7,6 +7,8 @@ import { useState, useEffect } from "react";
 // TODO - add the navbar links
 // TODO - add the navbar logo
 
+const ImageCss = { maxWidth: "100%", height: "auto" };
+
 export default function Nav({ children }) {
   const route = useRouter().route.slice(1);
   const router = useRouter();
@@ -72,71 +74,69 @@ export default function Nav({ children }) {
     setNavOpen(false);
   };
 
-  return (
-    <>
-      <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <div className="me-2" style={{ height: "60px" }}>
-            <h1 className="m-0">
-              <span hidden={true}>Cartlands Independent, Kings Heath Park</span>
-            </h1>
-            <Link href="/">
-              <div style={{ width: "140px", height: "60px" }}>
-                <Image
-                  src={logo}
-                  alt="Cartlands Independent Logo"
-                  width={280}
-                  height={120}
-                  className="navbar-brand"
-                  priority={true}
-                />
-              </div>
-            </Link>
-          </div>
-          <button
-            id="toggleBtn"
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarToggler"
-            aria-controls="navbarToggler"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            onClick={toggleClick}
-          >
-            <div className="nav-icon">
-              <div></div>
+  return <>
+    <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <div className="me-2" style={{ height: "60px" }}>
+          <h1 className="m-0">
+            <span hidden={true}>Cartlands Independent, Kings Heath Park</span>
+          </h1>
+          <Link href="/" legacyBehavior>
+            <div style={{ width: "140px", height: "60px" }}>
+              <Image
+                src={logo}
+                alt="Cartlands Independent Logo"
+                width={140}
+                height={60}
+                className={`navbar-brand ${ImageCss}`}
+                priority={true}
+              />
             </div>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarToggler">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-capitalize playfair-font">
-              {navItems.map((link) => {
-                let x = link.href.slice(1);
-                let activeClass =
-                  x == route
-                    ? { class: "nav-link active" }
-                    : { class: "nav-link" };
-                return (
-                  <li className="nav-item me-2" key={link.name}>
-                    <Link href={link.href}>
-                      <a
-                        className={activeClass.class}
-                        onClick={linkClick}
-                        //aria-current={home.aria}
-                        //aria-current="page"
-                      >
-                        {link.name}
-                      </a>
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+          </Link>
         </div>
-      </nav>
-      {children}
-      <Footer />
-    </>
-  );
+        <button
+          id="toggleBtn"
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarToggler"
+          aria-controls="navbarToggler"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={toggleClick}
+        >
+          <div className="nav-icon">
+            <div></div>
+          </div>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarToggler">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-capitalize playfair-font">
+            {navItems.map((link, key) => {
+              let x = link.href.slice(1);
+              let activeClass =
+                x == route
+                  ? { class: "nav-link active" }
+                  : { class: "nav-link" };
+              return (
+                <li className="nav-item me-2" key={key}>
+                  <Link
+                    href={link.href}
+                    className={activeClass.class}
+                    //aria-current={home.aria}
+                    //aria-current="page"
+                    onClick={linkClick}>
+
+                    {link.name}
+
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </nav>
+    {children}
+    <Footer />
+  </>;
 }
